9b257e0eeee472207d6ab99bb543f765b0b8e0a2
raisyaazzahra1705-art <raisyaazzahra1705@gmail.com>
2026-05-20 17:59:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,125 +1,28 @@
-const video = document.getElementById("webcam");
-const overlay = document.getElementById("overlay");
-const ctx = overlay.getContext("2d");
+const tombol=document.getElementById("btnKamera");
+const video=document.getElementById("video");
+const status=document.getElementById("status");
 
-const statusText = document.getElementById("status");
-const startBtn = document.getElementById("btn-init");
+tombol.addEventListener("click", aktifkanKamera);
 
-startBtn.addEventListener("click", startCamera);
+async function aktifkanKamera(){
 
-async function startCamera() {
+try{
 
-    try {
+const stream=await navigator.mediaDevices.getUserMedia({
+video:true
+});
 
-        const stream =
-        await navigator.mediaDevices.getUserMedia({
+video.srcObject=stream;
 
-            video: true,
-            audio: false
-        });
+status.innerHTML="STATUS : KAMERA AKTIF";
 
-        video.srcObject = stream;
+}
+catch(error){
 
-        await video.play();
+status.innerHTML="STATUS : GAGAL AKSES KAMERA";
 
-        statusText.innerText =
-        "STATUS : KAMERA AKTIF";
+console.log(error);
 
-        startBtn.style.display = "none";
-
-        drawBox();
-
-    } catch (error) {
-
-        console.log(error);
-
-        statusText.innerText =
-        "KAMERA GAGAL DIAKSES";
-    }
 }
 
-function drawBox() {
-
-    ctx.strokeStyle = "#00ff00";
-
-    ctx.lineWidth = 4;
-
-    ctx.strokeRect(
-        180,
-        100,
-        250,
-        220
-    );
-
-    ctx.fillStyle = "#00ff00";
-
-    ctx.font = "bold 20px Arial";
-
-    ctx.fillText(
-        "Objek Terdeteksi",
-        180,
-        90
-    );
-}const video = document.getElementById("webcam");
-const overlay = document.getElementById("overlay");
-const ctx = overlay.getContext("2d");
-
-const statusText = document.getElementById("status");
-const startBtn = document.getElementById("btn-init");
-
-startBtn.addEventListener("click", startCamera);
-
-async function startCamera() {
-
-    try {
-
-        const stream =
-        await navigator.mediaDevices.getUserMedia({
-
-            video: true,
-            audio: false
-        });
-
-        video.srcObject = stream;
-
-        await video.play();
-
-        statusText.innerText =
-        "STATUS : KAMERA AKTIF";
-
-        startBtn.style.display = "none";
-
-        drawBox();
-
-    } catch (error) {
-
-        console.log(error);
-
-        statusText.innerText =
-        "KAMERA GAGAL DIAKSES";
-    }
-}
-
-function drawBox() {
-
-    ctx.strokeStyle = "#00ff00";
-
-    ctx.lineWidth = 4;
-
-    ctx.strokeRect(
-        180,
-        100,
-        250,
-        220
-    );
-
-    ctx.fillStyle = "#00ff00";
-
-    ctx.font = "bold 20px Arial";
-
-    ctx.fillText(
-        "Objek Terdeteksi",
-        180,
-        90
-    );
 }
